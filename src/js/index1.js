22a6ea710c0a32706069b0df8b0e07e79c94060e
Anastasia Burdina <naste-f@yandex.ru>
2020-10-27 14:17:19 +0300

    11

--- src/js/index1.js
+++ src/js/index1.js
@@ -8,16 +8,15 @@ function init() {
   });
 
   const myClusterer = new ymaps.Clusterer({
-    groupByCoordinates: true,
-      clusterDisableClickZoom: true,
-      clusterOpenBalloonOnClick: false
+    groupByCoordinates: false,
+    clusterDisableClickZoom: true,
+    clusterOpenBalloonOnClick: false
   });
 
   myMap.geoObjects.add(myClusterer);
 
   let placemark = '';
   let myGeoObjects = [];
-  //myClusterer.add(myGeoObjects);
 
   addExistingPlacemarks();
 
@@ -27,30 +26,26 @@ function init() {
 
   myMap.events.add('click', (e) => {
     coords = e.get('coords');
+    console.log
     onClick(coords);
   })
   
   function createPlacemark(coords) {
     placemark = new ymaps.Placemark(coords);
-    myGeoObjects.push(placemark)
-    console.log(myGeoObjects)
-    myClusterer.add(myGeoObjects);
-
-    placemark.events.add('click', (e) => {
+    //if (!myGeoObjects.includes(placemark)) {
+      myGeoObjects.push(placemark)
+      myClusterer.add(myGeoObjects);
+    //}
     
+    console.log(myGeoObjects)
+    
+    placemark.events.add('click', (e) => {
       const placemarkCoords = e.get('target').geometry.getCoordinates();
-      console.log(placemarkCoords);
+
+      console.log(e)
       onPlacemarkClick(placemarkCoords)
     })
   }
-
-  //function getPlacemarkCoordinates(mark) {
-  //  placemark.events.add('click', () => {  
-  //    const placemarkCoords = placemark.geometry.getCoordinates();
-  //    console.log(placemarkCoords);
-  //  })
-  //  return placemarkCoords;
-  //}
 
   function openBalloon(coords, content) {
     myMap.balloon.open(coords, content);
@@ -80,13 +75,12 @@ function init() {
   function createForm(coords, reviews) {
     const root = document.createElement('div');
     root.innerHTML = formTemplate;
-
-    const reviewList = root.querySelector('.reviews')
     const reviewForm = root.querySelector('#review-form')
     reviewForm.dataset.coords = JSON.stringify(coords);
 
     if (reviews) {  
       for (const review of reviews) {
+        const reviewList = root.querySelector('.reviews')
         const div = document.createElement('div');
         div.classList.add('review-item');
         div.innerHTML = `
@@ -96,16 +90,16 @@ function init() {
 
         reviewList.appendChild(div);
 
-        if (review.coord == coords) {
-        const div = document.createElement('div');
-        div.classList.add('review-item');
-        div.innerHTML = `
-        <div>${review.review.name} (${review.review.place})</div>
-        <div><i>${review.review.text}</i></div>
-        `;
+        //if (review.coord == coords) {
+        //const div = document.createElement('div');
+        //div.classList.add('review-item');
+        //div.innerHTML = `
+        //<div>${review.review.name} (${review.review.place})</div>
+        //<div><i>${review.review.text}</i></div>
+        //`;
 
-        reviewList.appendChild(div);
-        }
+        //reviewList.appendChild(div);
+        //}
       }
     }
 
@@ -113,29 +107,31 @@ function init() {
   }
 
   function onClick(coords) {
-    //const list = JSON.parse(localStorage.getItem('markers'));
+    
     const form = createForm(coords);
-
-    openBalloon(coords);
-    //setBalloonContent(form.innerHTML); 
-
-   // const address = ymaps.geocode(coords).geoObjects.get(0).getAddressLine();
-
+    openBalloon(coords, form.innerHTML);
   }
 
   function onPlacemarkClick(coords) {
     const list = JSON.parse(localStorage.getItem('markers'));
 
     for (const mark of list) {
+      const shortList = []; 
+
       console.log(mark.coord)
       console.log(coords)
-      if (mark.coord === coords){
-        const form = createForm(coords, mark);
 
-        console.log(list)
+      if (JSON.stringify(mark.coord) == JSON.stringify(coords)) {
+        console.log(mark.coord)
+        shortList.push(mark);
+
+        const form = createForm(coords, shortList);
+
+        console.log(shortList)
     
         openBalloon(coords, form.innerHTML);
         //setBalloonContent(form.innerHTML); 
+        // const address = ymaps.geocode(coords).geoObjects.get(0).getAddressLine();
       }
     }
     
@@ -165,15 +161,12 @@ function init() {
   
       markers.push(data);
   
-      try {
-        localStorage.setItem('markers', JSON.stringify(markers))
+      localStorage.setItem('markers', JSON.stringify(markers));
+      if (!myGeoObjects.includes(coords)){
         createPlacemark(coords)
-        closeBalloon()
-      } catch (e) {
-        const formError = document.querySelector('.form-error');
-        formError.innerText = e.message;
-        console.log(e)
       }
+      closeBalloon()
+      
     }
   })
 }
